6ff0737d1603949707892fe63c54c0b6720174d6
wostrau <a.ostrovtsov@gmail.com>
2023-10-19 22:38:08 +0200

    added shadow DOM template for tooltip

--- tooltip.js
+++ tooltip.js
@@ -2,24 +2,49 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
+        this._tooltipText = 'Some dummy tooltip text.';
+        this.attachShadow({ mode: 'open' });
+        // const template = document.querySelector('#tooltip-template');
+        // this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.innerHTML = `
+            <style>
+                div {
+                    background-color: black;
+                    color: white;
+                    position: absolute;
+                    z-index: 10;
+                }
+            </style>
+            <slot>Some Default</slot>
+            <span> (?)</span>
+        `;
     }
 
     connectedCallback() {
-        const tooltipIcon = document.createElement('span');
-        tooltipIcon.textContent = ' (?)';
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
+        // const tooltipIcon = document.createElement('span');
+        // tooltipIcon.textContent = ' (?)';
+        const tooltipIcon = this.shadowRoot.querySelector('span');
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
         tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
-        this.appendChild(tooltipIcon);
+        this.shadowRoot.appendChild(tooltipIcon);
+        this.style.position = 'relative';
     }
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is tooltip text!';
-        this.appendChild(this._tooltipContainer);
+        this._tooltipContainer.textContent = this._tooltipText;
+        // this._tooltipContainer.style.backgroundColor = 'black';
+        // this._tooltipContainer.style.color = 'white';
+        // this._tooltipContainer.style.position = 'absolute';
+        // this._tooltipContainer.style.zIndex = '10';
+        this.shadowRoot.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
-        this.removeChild(this._tooltipContainer);
+        this.shadowRoot.removeChild(this._tooltipContainer);
     }
 }
 
