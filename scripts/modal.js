c12f6b00c271f12bbbedfde336adaf2fb64ae346
wostrau <a.ostrovtsov@gmail.com>
2023-10-20 18:49:03 +0200

    added custom modal web component

--- scripts/modal.js
+++ scripts/modal.js
@@ -19,99 +19,87 @@ class Modal extends HTMLElement {
 
             :host([opened]) #backdrop,
             :host([opened]) #modal {
-                opacity: 1;
-                pointer-events: all;
+              opacity: 1;
+              pointer-events: all;
             }
 
             :host([opened]) #modal {
-                top: 15vh;
+              top: 15vh;
             }
 
             #modal {
-                position: fixed;
-                top: 10vh;
-                left: 25%;
-                width: 50%;
-                z-index: 100;
-                background: white;
-                border-radius: 3px;
-                box-shadow: 0 2px 8px rgba(0,0,0,0.26);
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                opacity: 0;
-                pointer-events: none;
-                transition: all 0.3s ease-out;
+              position: fixed;
+              top: 10vh;
+              left: 25%;
+              width: 50%;
+              z-index: 100;
+              background: white;
+              border-radius: 3px;
+              box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+              display: flex;
+              flex-direction: column;
+              justify-content: space-between;
+              opacity: 0;
+              pointer-events: none;
+              transition: all 0.3s ease-out;
             }
 
             header {
-                padding: 1rem;
-                border-bottom: 1px solid #ccc;
+              padding: 1rem;
+              border-bottom: 1px solid #ccc;
             }
 
             ::slotted(h1) {
-                font-size: 1.25rem;
-                margin: 0;
+              font-size: 1.25rem;
+              margin: 0;
             }
 
             #main {
-                padding: 1rem;
+              padding: 1rem;
             }
 
             #actions {
-                border-top: 1px solid #ccc;
-                padding: 1rem;
-                display: flex;
-                justify-content: flex-end;
+              border-top: 1px solid #ccc;
+              padding: 1rem;
+              display: flex;
+              justify-content: flex-end;
             }
 
             #actions button {
-                margin: 0 0.25rem;
+              margin: 0 0.25rem;
             }
         </style>
         <div id="backdrop"></div>
         <div id="modal">
             <header>
-                <slot name="title">Please Confirm Payment</slot>
+              <slot name='title'>Please Confirm Payment</slot>
             </header>
-            <section id="main">
-                <slot></slot>
+            <section id='main'>
+              <slot></slot>
             </section>
-            <section id="actions">
-                <button id="cancel-btn">Cancel</button>
-                <button id="confirm-btn">Okay</button>
+            <section id='actions'>
+              <button id='cancel-btn'>Cancel</button>
+              <button id='confirm-btn'>Confirm</button>
             </section>
         </div>
     `;
+
     const slots = this.shadowRoot.querySelectorAll('slot');
     slots[1].addEventListener('slotchange', event => {
       console.dir(slots[1].assignedNodes());
     });
+
     const backdrop = this.shadowRoot.querySelector('#backdrop');
     const cancelButton = this.shadowRoot.querySelector('#cancel-btn');
     const confirmButton = this.shadowRoot.querySelector('#confirm-btn');
+
     backdrop.addEventListener('click', this._cancel.bind(this));
     cancelButton.addEventListener('click', this._cancel.bind(this));
     confirmButton.addEventListener('click', this._confirm.bind(this));
+
     // cancelButton.addEventListener('cancel', () => {
-    //   console.log('Cancel inside the component');
+    //   console.log('Cancel inside component!');
     // });
-  }
-
-  attributeChangedCallback(name, oldValue, newValue) {
-    if (this.hasAttribute('opened')) {
-      this.isOpen = true;
-      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
-      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
-      // this.shadowRoot.querySelector('#modal').style.opacity = 1;
-      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
-    } else {
-      this.isOpen = false;
-    }
-  }
-
-  static get observedAttributes() {
-    return ['opened'];
   }
 
   open() {
@@ -137,6 +125,24 @@ class Modal extends HTMLElement {
     const confirmEvent = new Event('confirm');
     this.dispatchEvent(confirmEvent);
   }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (this.hasAttribute('opened')) {
+      this.isOpen = true;
+
+      // this.shadowRoot.querySelector('#backdrop').style.opacity = 1;
+      // this.shadowRoot.querySelector('#backdrop').style.pointerEvents = 'all';
+
+      // this.shadowRoot.querySelector('#modal').style.opacity = 1;
+      // this.shadowRoot.querySelector('#modal').style.pointerEvents = 'all';
+    } else {
+      this.isOpen = false;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['opened'];
+  }
 }
 
-customElements.define('uc-modal', Modal);
+customElements.define('aa-modal', Modal);
